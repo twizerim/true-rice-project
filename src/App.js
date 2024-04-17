706ba417e4a6bdc4dc2aed921d39603f323da90b
ATLP <atlp@ATLPs-MacBook-Pro.local>
2024-04-17 21:38:12 +0200

    single

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Productpage from './pages/product';
 import Admindashboard from './pages/admindash';
 import Farmerdashboard from './pages/farmerdashboard';
 import Impact from './pages/impact';
+import Singleproduct from './pages/singleproduct';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/adminpanel' element={<Admindashboard/>}/>
         <Route path='/farmerpanel' element={<Farmerdashboard/>}/>
         <Route path='/impact' element={<Impact/>}/>
+        <Route path='/single' element={<Singleproduct/>}/>
       </Routes>
     </Router>
   );
--- src/pages/singleproduct.jsx
+++ src/pages/singleproduct.jsx
@@ -1,12 +1,39 @@
 
-const { createRoot } = ReactDOM;
+import React from 'react';
+import '../sass/main.scss'
+import Navbar from '../component/header';
+import { Image } from 'antd';
 
-const {  Image  } = antd;
-const Singleproduct = () => (
-  <Image
-    width={200}
-    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-  />
-);
+function Singleproduct() {
+      return (
+        <>
+          <Navbar/>
+           <div className="single-production">
+              <div className="image-contain">
+                   <div className="image">
+                   <Image
+                      width={500}
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1oUc4hyMH-UZu-ZXFlVGUsj9VtVJsAwD0BQ&s"
+                   />
+                       <div className="calculator">
+                         <button>-</button>
+                            <h1>0</h1>
+                         <button>+</button>
+                       </div>
+                   </div>
+                   <div className="description">
+                     <h1>The name of the product</h1>
+                      <h2>title of product</h2>
+                       <h3>Price : <p>200</p>Rwf</h3>
+                      <p>Quotinty available: <span>599999</span>Kg</p> 
+                       <span>It better to buy this project because it support your body</span>
+                       <h5 className='cost'>Totol cost:<h6>2002</h6>Rwf</h5>
+                   </div>
+              </div>
+              <h1>Form to fill to buy product</h1>
+           </div>
+        </>
+      )
+};
+
 export default Singleproduct;
-
